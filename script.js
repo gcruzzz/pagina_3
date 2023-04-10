9636ef325804eb1d4a8580e311d238b909b9333b
gcruzzz <128652313+gcruzzz@users.noreply.github.com>
2023-04-09 21:36:39 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const simButton = document.querySelector('#sim');
+const claroButton = document.querySelector('#claro');
+
+simButton.addEventListener('click', () => {
+	window.location.href = 'https://exemplo.com/qr-code';
+});
+
+claroButton.addEventListener('click', () => {
+	window.location.href = 'https://exemplo.com/qr-code';
+});
